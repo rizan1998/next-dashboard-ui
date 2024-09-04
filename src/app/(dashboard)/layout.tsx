75b0810ecedf31796a17064378d2e4f474c875be
rizan1998 <rizanalfalah@gmail.com>
2024-09-04 09:33:27 +0700

    unfinish navbar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "@/components/Menu";
+
 export default function DashboardLayout({
   children,
 }: Readonly<{
@@ -6,9 +10,15 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* LEFT */}
-      <div className="w-[14%] md:-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">l</div>
+      <div className="w-[14%] md:-[8%] lg:w-[16%] xl:w-[14%]  p-4">
+        <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
+          <Image src="/logo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">SchoolLama</span>
+        </Link>
+        <Menu/>
+      </div>
       {/* RIGHT */}
-      <div className="w-[86%] md:w-[92%] lg:w[84%] xl:w-[86%] bg-blue-200">r</div>
+      <div className="w-[86%] md:w-[92%] lg:w[84%] xl:w-[86%] ">r</div>
     </div>
   );
 }
